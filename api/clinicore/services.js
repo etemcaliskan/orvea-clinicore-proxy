d24d832371fcc038fc4c0cfdc86f01fe6c9c5424
etemcaliskan <etem.caliskan@me.com>
2026-04-20 03:02:17 +0200

    Update services.js

--- api/clinicore/services.js
+++ api/clinicore/services.js
@@ -7,10 +7,6 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  if (req.method !== "GET") {
-    return res.status(405).json({ error: "Method not allowed" });
-  }
-
   try {
     const token = process.env.CLINICORE_WAPI_TOKEN;
 
